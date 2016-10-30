6873fc9b0721999705723d7f568730b5efea020c
Filip Procházka <filip@prochazka.su>
2016-10-30 13:16:25 +0100

    typo

--- soccer-records-model/src/main/java/cz/muni/fi/pa165/config/ApplicationConfig.java
+++ soccer-records-model/src/main/java/cz/muni/fi/pa165/config/ApplicationConfig.java
@@ -1,8 +1,6 @@
 package cz.muni.fi.pa165.config;
 
 import org.springframework.context.annotation.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @ComponentScan(basePackages = "cz.muni.fi.pa165")
