ca0b0b11895a2272e4e3c4ba114ea154a394d902
Tomáš Šmíd <smid.thomas@gmail.com>
2016-11-23 10:42:41 +0100

    Modifies MatchTimeCollisionException to be more relevant, adds getters

--- soccer-records-model/src/main/java/cz/muni/fi/pa165/team/exceptions/MatchTimeCollisionException.java
+++ soccer-records-model/src/main/java/cz/muni/fi/pa165/team/exceptions/MatchTimeCollisionException.java
@@ -11,31 +11,35 @@ public class MatchTimeCollisionException extends RuntimeException
 {
     private final UUID matchId;
 
-    private final String homeTeam;
-
-    private final String awayTeam;
+    private final UUID team;
 
     private final Date startTime;
 
-    public MatchTimeCollisionException(
-        UUID matchId,
-        String homeTeam,
-        String awayTeam,
-        Date startTime
-    )
+    public MatchTimeCollisionException(UUID matchId, UUID team, Date startTime)
     {
-        super(String.format("There cannot be changed the start time of the match " +
-                "%s with home team %s and away team %s to new value %s, because " +
-                "there already exists another match with same start time in which " +
-                "one of these teams plays",
+        super(String.format("There cannot be changed the start time of the match %s with team %s to new value %s, " +
+                "because there already exists another match with that same start time and same team",
             matchId.toString(),
-            homeTeam,
-            awayTeam,
+            team.toString(),
             new SimpleDateFormat("dd/MM/YYYY hh:mm:ss").format(startTime)));
 
         this.matchId = matchId;
-        this.homeTeam = homeTeam;
-        this.awayTeam = awayTeam;
+        this.team = team;
         this.startTime = new Date(startTime.getTime());
+    }
+
+    public UUID getMatchId()
+    {
+        return matchId;
+    }
+
+    public UUID getTeam()
+    {
+        return team;
+    }
+
+    public Date getStartTime()
+    {
+        return new Date(startTime.getTime());
     }
 }
