34e21e2f634c9e2407c60c903a95a9e81bdfe681
Filip Procházka <filip@prochazka.su>
2016-11-25 21:58:42 +0100

    use Mockito in UserServiceTest

--- soccer-records-model/src/test/java/cz/muni/fi/pa165/user/UserServiceTest.java
+++ soccer-records-model/src/test/java/cz/muni/fi/pa165/user/UserServiceTest.java
@@ -1,9 +1,13 @@
 package cz.muni.fi.pa165.user;
 
 import cz.muni.fi.pa165.user.exceptions.UserWithSameEmailIsAlreadyRegisteredException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.testng.annotations.Test;
 
+import static org.mockito.Mockito.doReturn;
 import static org.testng.Assert.*;
 
 /**
@@ -15,7 +19,11 @@ public class UserServiceTest
     @Test
     public void testCreateUser() throws Exception
     {
-        UserService userService = getUserService();
+        PasswordEncoder passwordEncoder = mockPasswordEncoder();
+        UserService userService = new UserService(passwordEncoder);
+
+        doReturn("secret-hash").when(passwordEncoder)
+            .encode("heslo");
 
         User user = userService.createUser("filip@prochazka.su", null, "heslo");
         assertEquals("filip@prochazka.su", user.getEmail());
@@ -26,7 +34,11 @@ public class UserServiceTest
     @Test
     public void testCreateUserWithAlreadyExistingUserWithSameEmailException() throws Exception
     {
-        UserService userService = getUserService();
+        PasswordEncoder passwordEncoder = mockPasswordEncoder();
+        UserService userService = new UserService(passwordEncoder);
+
+        doReturn("secret-hash").when(passwordEncoder)
+            .encode("heslo");
 
         User sameEmailUser = userService.createUser("filip@prochazka.su", null, "heslo");
         try {
@@ -44,15 +56,32 @@ public class UserServiceTest
     )
     public void testCreateUserWithAlreadyExistingUserWithDifferentEmailException() throws Exception
     {
-        UserService userService = getUserService();
+        PasswordEncoder passwordEncoder = mockPasswordEncoder();
+        UserService userService = new UserService(passwordEncoder);
+
+        doReturn("secret-hash").when(passwordEncoder)
+            .encode("heslo");
 
         User sameEmailUser = userService.createUser("filip@prochazka.nejsu", null, "heslo");
         userService.createUser("filip@prochazka.su", sameEmailUser, "heslo");
     }
 
-    private UserService getUserService()
+    private PasswordEncoder mockPasswordEncoder()
     {
-        return new UserService(new BCryptPasswordEncoder(13));
+        return Mockito.mock(PasswordEncoder.class, new DefaultMockitoThrowingAnswer());
+    }
+
+    private class DefaultMockitoThrowingAnswer implements Answer<Object>
+    {
+        @Override
+        public Object answer(InvocationOnMock invocation) throws Throwable
+        {
+            throw new UnsupportedOperationException(String.format(
+                "Method \"%s\" not stubbed on %s",
+                invocation.getMethod().getName(),
+                invocation.getMock().getClass().getName()
+            ));
+        }
     }
 
 }
