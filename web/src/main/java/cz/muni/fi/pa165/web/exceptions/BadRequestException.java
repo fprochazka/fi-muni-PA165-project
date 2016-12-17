c90e4cbdecea9b078d479ff3abe3c092660aaf8a
Filip Procházka <filip@prochazka.su>
2016-12-17 23:21:59 +0100

    custom web exceptions

---
+++ web/src/main/java/cz/muni/fi/pa165/web/exceptions/BadRequestException.java
@@ -0,0 +1,22 @@
+package cz.muni.fi.pa165.web.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Filip Prochazka <filip@prochazka.su>
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException
+{
+
+    public BadRequestException(String message)
+    {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+}
