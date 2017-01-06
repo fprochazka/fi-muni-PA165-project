57b66aa01eea4d4b690df9a871e951e8d9b11a96
Tomáš Šmíd <smid.thomas@gmail.com>
2017-01-06 02:43:05 +0100

    Removes all previous methods and adds methods viewListPlayed and viewListPlanned

--- web/src/main/java/cz/muni/fi/pa165/team/match/MatchController.java
+++ web/src/main/java/cz/muni/fi/pa165/team/match/MatchController.java
@@ -1,19 +1,12 @@
 package cz.muni.fi.pa165.team.match;
 
-import cz.muni.fi.pa165.response.RedirectResponse;
-import cz.muni.fi.pa165.team.Team;
-import cz.muni.fi.pa165.team.TeamPlayer;
 import cz.muni.fi.pa165.team.TeamPlayerRepository;
 import cz.muni.fi.pa165.team.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.time.format.DateTimeFormatter;
-import java.util.*;
 
 /**
  * @author Tomas Smid <smid.thomas@gmail.com>
@@ -44,68 +37,17 @@ public class MatchController
     }
 
     @RequestMapping(value = "/matches", method = RequestMethod.GET)
-    public ModelAndView viewMatchList()
+    public ModelAndView viewListPlayed()
     {
-        List<Integer> homeTeamsGoals = new ArrayList<>();
-        List<Integer> awayTeamsGoals = new ArrayList<>();
-        Collection<TeamMatch> playedMatches = teamMatchFacade.findAllPlayedMatches();
-        Collection<TeamMatch> plannedMatches = teamMatchFacade.findAllPlannedMatches();
-        ModelAndView modelAndView = new ModelAndView("team/match/list");
-
-        for (TeamMatch pmatch : playedMatches) {
-            homeTeamsGoals.add(teamMatchFacade.findAllGoalByHomeTeamInMatch(pmatch.getId()).size());
-            awayTeamsGoals.add(teamMatchFacade.findAllGoalByAwayTeamInMatch(pmatch.getId()).size());
-        }
-        modelAndView.addObject("playedMatches", playedMatches);
-        modelAndView.addObject("plannedMatches", plannedMatches);
-        modelAndView.addObject("homeTeamsGoals", homeTeamsGoals);
-        modelAndView.addObject("awayTeamsGoals", awayTeamsGoals);
-        modelAndView.addObject("formatter", DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
-
-        return modelAndView;
+        return new ModelAndView("team/match/played_list")
+            .addObject("pmContainer", teamMatchFacade.preparePlayedMatchesForList());
     }
 
-    @RequestMapping(value = "/match/new", method = RequestMethod.GET)
-    public ModelAndView viewNewMatchForm()
+    @RequestMapping(value = "/matches/upcomming", method = RequestMethod.GET)
+    public ModelAndView viewListPlanned()
     {
-        MatchRequest matchRequest = new MatchRequest();
-        ModelAndView modelAndView = new ModelAndView("team/match/mform");
-        Collection<Team> allTeams = teamRepository.findAll();
-
-        matchRequest.setTarget("/match/create");
-        modelAndView.addObject("homeTeams", allTeams);
-        modelAndView.addObject("awayTeams", allTeams);
-        modelAndView.addObject("matchRequest", matchRequest);
-
-        return modelAndView;
+        return new ModelAndView("team/match/upcomming_list")
+            .addObject("pmContainer", teamMatchFacade.preparePlayedMatchesForList());
     }
 
-    @RequestMapping(value = "/match/{id}/delete", method = RequestMethod.POST)
-    public ModelAndView deleteMatch(@PathVariable("id") UUID id) throws Exception
-    {
-        teamMatchFacade.deleteMatch(id);
-        return new RedirectResponse("/matches");
-    }
-
-    @RequestMapping(value = "/match/{id}")
-    public ModelAndView viewMatchDetail(@PathVariable("id") UUID id)
-    {
-        TeamMatch match = teamMatchFacade.getMatchById(id);
-        Collection<TeamMatchGoal> homeGoals = teamMatchGoalRepository.findAllGoalsByTeamInMatch(
-            match.getId(), match.getHomeTeam().getId());
-        Collection<TeamMatchGoal> awayGoals = teamMatchGoalRepository.findAllGoalsByTeamInMatch(
-            match.getId(), match.getAwayTeam().getId());
-        Collection<TeamPlayer> homePlayers = teamPlayerRepository.findTeamPlayerByTeam(match.getHomeTeam());
-        Collection<TeamPlayer> awayPlayers = teamPlayerRepository.findTeamPlayerByTeam(match.getAwayTeam());
-        ModelAndView modelAndView = new ModelAndView("team/match/detail");
-
-        modelAndView.addObject("match", match);
-        modelAndView.addObject("formatter", DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
-        modelAndView.addObject("homeGoals", homeGoals);
-        modelAndView.addObject("awayGoals", awayGoals);
-        modelAndView.addObject("homePlayers", homePlayers);
-        modelAndView.addObject("awayPlayers", awayPlayers);
-
-        return modelAndView;
-    }
 }
